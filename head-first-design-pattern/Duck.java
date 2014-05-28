034636a32b7b46c55531117f21fe7b68371aab2f
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2014-05-29 03:47:01 +0900

    デザパタ1章

---
+++ head-first-design-pattern/Duck.java
@@ -0,0 +1,20 @@
+public abstract class Duck {
+    FlyBehavior flyBehavior;
+    QuackBehavior quackBehavior;
+    public Duck() {
+    }
+
+    public abstract void display();
+
+    public void performFly() {
+        flyBehavior.fly();
+    }
+
+    public void performQuack() {
+        quackBehavior.quack();
+    }
+
+    public void swim() {
+        System.out.println("すべてのかもは浮かびます。おとりの鴨でも！");
+    }
+}
---
+++ head-first-design-pattern/Sqeak.java
@@ -0,0 +1,5 @@
+public class Sqeak implements QuackBehavior {
+    public void quack() {
+        System.out.println("キューキュー");
+    }
+}
